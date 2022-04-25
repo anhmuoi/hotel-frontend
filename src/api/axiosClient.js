794d8dbd4d450f4542d946df8818926b709e0b9c
anhmuoi <anhmuoi200147@gmail.com>
2022-04-25 20:30:28 +0700

    env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import StorageKeys from '../constants/storage-key.js';
 const { default: axios } = require("axios");
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://lovedatingfriends.xyz/api',
   headers: {
     "Content-Type": "application/json",
   },
